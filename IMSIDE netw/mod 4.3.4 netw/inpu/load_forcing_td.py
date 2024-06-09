44e9fac8b1a2af97b7c8183243ee0915b4bed0aa
JesseKoppenaal <jesse.koppenaal@gmail.com>
2024-06-09 11:25:27 +0200

    mri5

--- IMSIDE netw/mod 4.3.4 netw/inpu/load_forcing_td.py
+++ IMSIDE netw/mod 4.3.4 netw/inpu/load_forcing_td.py
@@ -13,7 +13,7 @@ def forc_RMDJ():
     #os.chdir('../../../../Downscaling_Salt_Intrusion/predictions')
     
     ssp = 'SSP5-8.5'
-    member = 'CMCC'
+    member = 'MRI'
     
     # xl_Tiel = pd.read_excel('ensemble_average_Tiel_'+ssp+'.xlsx')
     # xl_Megen = pd.read_excel('ensemble_average_Megen_'+ssp+'.xlsx')
